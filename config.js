aa44dcb97787e45a0b23cede47a4b5c50141f878
atlasego2 <102427923+atlasego2@users.noreply.github.com>
2024-02-04 17:11:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -21,15 +21,15 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94772108460", "+94772108460"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94772108460", "+94772108460"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94772108460"];
-global.OwnerName = "KUSHAN";
-global.BotName = "DARK SHAM";
-global.packname = "🦋 DARK SHAN 🦋";                             //Do not change.
-global.author = "KUSHAN";                               //Do not change.
+global.Owner = ["+905012560110", "+3807300322023"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+905012560110", "+3807300322023"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+905012560110"];
+global.OwnerName = "EGOFFİCE";
+global.BotName = "EGOFFİCE";
+global.packname = "EGOFFİCE";                             //Do not change.
+global.author = "EGOFFİCE";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
-global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
+global.SupportGroupLink = "https://chat.whatsapp.com/IO6wVVySnxmBI7AQqHsvcx";
 
 
 //
@@ -41,12 +41,12 @@ global.openAiAPI = "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";
 
 
 //
-global.location = "mathara, Sri Lankan";
+global.location = "İSTANBUL, TR";
 global.reactmoji = "💞";
 global.themeemoji = "😂";
 global.vidmenu = { url: 'https://tenor.com/en-CA/view/anonymous-hacker-smooth-gif-10171911193797826570%9D%90%95-12-14' };
-global.websitex = "https://github.com/CYBER-x-SACHIYA-SL-MD-BOT/CYBER-PANDA-MD.V.0.4";
-global.lolhuman = "kushan";
+global.websitex = "https://github.com/atlasego2/bot";
+global.lolhuman = "EGOFFİCE";
 
 
 //
